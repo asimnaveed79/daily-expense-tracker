e8f8c01d0dd69b8fe3cbc882e333709ac83b05a5
asimnaveed79 <asim.naveed@gmail.com>
2025-04-12 17:57:10 +0400

    Update Firebase config to match live project

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyB9ZEVK8fGz_JK25ACj6T3v5WOb3n38Nn4",
-  authDomain: "daily-expense-tracker-e2fd1.firebaseapp.com",
-  projectId: "daily-expense-tracker-e2fd1",
-  storageBucket: "daily-expense-tracker-e2fd1.appspot.com",
-  messagingSenderId: "1012253140198",
-  appId: "1:1012253140198:web:0c152e54a03a383d783ab6"
+  apiKey: "AIzaSyD3jMdSfsb_aclnlN4AB6Nbf9IckJ16TrE",
+  authDomain: "daily-expense-tracker-8a732.firebaseapp.com",
+  projectId: "daily-expense-tracker-8a732",
+  storageBucket: "daily-expense-tracker-8a732.firebasestorage.app",
+  messagingSenderId: "20751590124",
+  appId: "1:20751590124:web:8475e9e959519eadcafbc9"
 };
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
